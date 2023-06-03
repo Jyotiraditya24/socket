22c5bb1f15b78c6ed579fb15ad20a69ca40bf209
jyotiraditya <jyotiraditya24@gmail.com>
2023-06-03 16:01:56 +0530

    Can send messages to backend from 2+ client

--- chatApp/src/App.jsx
+++ chatApp/src/App.jsx
@@ -1,14 +1,36 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import { socket } from "./socket";
 
 function App() {
-  useEffect(()=>{
-    socket.connect();
-  })
-  return <div>Hello done
+  const [data, setData] = useState("");
 
-  </div>;
+  useEffect(() => {
+    socket.connect();
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    socket.emit("chatMessage", data);
+    setData("");
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="chat"
+          className="bg-blue-200"
+          onChange={(e) => setData(e.target.value)}
+        />
+        <input
+          type="submit"
+          className="rounded-md mx-2 cursor-pointer bg-black text-white px-2 py-1"
+        />
+      </form>
+    </div>
+  );
 }
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -29,9 +29,12 @@ server.listen(3001, () => {
   console.log("Connected to " + 3001);
 });
 
-io.on("connection",(socket)=>{
-    console.log("a user connected");
-    socket.on("disconnect",()=>{
-        console.log("disconnected");
-    })
-})
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("disconnected");
+  });
+  socket.on("chatMessage", (data) => {
+    console.log(data);
+  });
+});
